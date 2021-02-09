fe23d925d4149d635a36c2e5dc74dcd7e3ffe2aa
wafaankoush99 <wafadirawe@gmail.com>
2021-02-09 14:44:58 +0200

    while loop added

--- js/app.js
+++ js/app.js
@@ -1,51 +1,51 @@
 
 
-
-// var isintersted = true;
-// console.log(FirstName + ',' + Age + 'years old, is interested' + isinterested)
-
-
-
 var UserName = prompt ('Whats your name?');
 console.log(UserName);
 
-// show an alert with input feid 
 
-alert('Welcome '+ UserName);
-document.write('Welcome ' + UserName );
+
+alert('Welcome '+ UserName  );
+document.write('Welcome ' + UserName  );
+
+
 
 var isinterested = prompt('Are you intersted yes or no?');
 
-if (isinterested== 'yes')
+if ( isinterested== 'yes')
+
 {
     alert('We hope this is useful for you ♥');
+
     var order = prompt("Do you want a weight loss program?");
 
+    if (order == 'yes') {
 
-    // while(order == "yes" ){
-    //     order = prompt("Are you vegetarian or normal?");
-    // }
-    
-    
-    var itemOrder = prompt("Are you vegetarian or normal?");
-    
-    
-    if (itemOrder == 'vegetarian')
-    {
-    itemOrder = '<img src="vegetarian.jpg"/>';
-    }
-    else if(itemOrder == "normal")
-    {
-    itemOrder = '<img src="normality.jpg"/>';
-    }
-    console.log(itemOrder)
-    
-    
+        
+        var order = prompt("Are you vegetarian or normal?");
+
+        while(order !== "vegetarian" &&  order !== "normal")
+        {
+            order = prompt("Are you vegetarian or normal?");
+        }
+        
+        
+        var itemOrder = '';
+        
+        if (itemOrder == 'vegetarian')
+        {
+        itemOrder = '<img src="vegetarian.jpg"/>';
+        }
+        else if(itemOrder == "normal")
+        {
+        itemOrder = '<img src="normality.jpg"/>';
+        }
+        
     
     
     var programme = prompt("Write 1 if you want to take a programme")
     
-    var result;
+    var result='';
     for (var i=0; i<programme ; i++)
     {
     result = result + itemOrder;
@@ -53,24 +53,37 @@ if (isinterested== 'yes')
     
     
     }
-    console.log(result)
-    document.write(result);
+    
+    
+    }
+    
+    
+
+
+    else {
+        alert('Thank you for visitng ♥');
+
+    }
+
 }
 
-else{
 
-alert('Thank you for visitng ♥');
+
+star = '<img src="star.jpg"/>'
+
+var stars = prompt("How many stars you give us from 1 to 5 ?");
+    
+var sum='';
+for (var i=0 ; i<stars ; i++)
+{
+sum = sum + star;
+
+
+
 }
 
-
-
-
-
-
-
-
-
-
+document.write(result);
+document.write(sum);
 
 
 
